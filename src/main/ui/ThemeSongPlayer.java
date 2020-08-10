db7543ff00d7b77ab5bf96adc3c6b1a1b845caa4
echau01 <echau01@students.cs.ubc.ca>
2020-08-10 00:43:47 -0700

    Update comments and lower volume of theme song

--- src/main/ui/ThemeSongPlayer.java
+++ src/main/ui/ThemeSongPlayer.java
@@ -4,11 +4,10 @@ import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 
-// This class plays the Tetris theme song on loop. Much of the code for this class comes from
-// https://stackoverflow.com/a/11025384/3335320
+// This class plays the Tetris theme song on loop.
 public class ThemeSongPlayer {
-    // The music file comes from https://commons.wikimedia.org/wiki/File:Tetris_theme.ogg
-    // I converted the MP3 to WAV.
+    // The music file comes from https://commons.wikimedia.org/wiki/File:Tetris_theme.ogg (CC BY-SA 3.0).
+    // The file was originally an MP3 file. I converted the MP3 to WAV using https://online-audio-converter.com/.
     private static final String THEME_SONG_FILE_PATH = "./data/tetrisTheme.wav";
 
     // This class uses the singleton pattern. This is the singleton instance.
@@ -16,8 +15,10 @@ public class ThemeSongPlayer {
 
     private Clip clip;
 
+    // EFFECTS: makes a new ThemeSongPlayer
     private ThemeSongPlayer() {}
 
+    // MODIFIES: this
     // EFFECTS: returns the singleton instance of ThemeSongPlayer.
     public static ThemeSongPlayer getInstance() {
         if (instance == null) {
@@ -32,14 +33,13 @@ public class ThemeSongPlayer {
     //          Throws LineUnavailableException if the "line" that plays the music is not available
     //          Throws UnsupportedAudioFileException if the file at THEME_SONG_FILE_PATH is not a valid audio file.
     public void startThemeOnLoop() throws IOException, LineUnavailableException, UnsupportedAudioFileException {
-        // The code for this method comes from https://stackoverflow.com/a/11025384/3335320
-        File musicFile = new File(THEME_SONG_FILE_PATH);
-        AudioInputStream stream = AudioSystem.getAudioInputStream(musicFile);
-        AudioFormat format = stream.getFormat();
-        DataLine.Info info = new DataLine.Info(Clip.class, format);
-        clip = (Clip) AudioSystem.getLine(info);
+        // The code for this method comes from https://stackoverflow.com/a/953752/3335320
+        AudioInputStream stream = AudioSystem.getAudioInputStream(new File(THEME_SONG_FILE_PATH));
+        clip = AudioSystem.getClip();
         clip.open(stream);
         clip.loop(Clip.LOOP_CONTINUOUSLY);
+        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        gainControl.setValue(-15.0f);
         clip.start();
     }
 
